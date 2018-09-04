0ce043aef7d20b8bb317b6fd90678ce91892e9ee
Věra Škopková <vera.skopkova@raventia.cz>
2018-09-04 17:21:26 +0200

    hotovo

---
+++ TreeVisualizer/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TreeVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1729, 957);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1741, 1029);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ TreeVisualizer/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeVisualizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Font font;
+        Bitmap image;
+        Graphics g;
+        Node actual;
+        int maxX = 1000;
+        int maxY = 1000;
+        int circleSize = 40; 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Text = fileName;
+                Node.maxDepth = -1;
+                Run();
+                //pictureBox1 = new PictureBox();
+                nodes.Clear();
+                Run();
+            }catch(FileNotFoundException)
+            {
+                MessageBox.Show("Soubor " + fileName + " nebyl nalezen. Zkuste jiný soubor.");
+            }
+        }
+
+        string fileName = "vstup.txt";
+
+        private void Run()
+        {
+            StreamReader sr = new StreamReader(fileName);
+            int vstup;
+            actual = new Node(null, true);
+            actual.from = 0;
+            actual.to = 2* maxX;
+            actual.x = maxX;
+            actual.y = (int)(-0.5 * Node.height);
+            actual.fake = true;
+            actual.depth = -1;
+            string number = "";
+
+            while ((vstup = sr.Read()) != -1)
+            {
+                char c = (char)vstup;
+                
+                switch (c)
+                {
+                    case '(':
+                        if (number != "")
+                        {
+                            setValue(number);
+                            number = "";
+                        }
+                        if  (actual.doleva)
+                        {
+                            actual.left = new Node(actual, true);
+                            actual.doleva = false;
+                            actual = actual.left;
+                            nodes.Add(actual);
+                        }
+                        else
+                        {
+                            actual.right = new Node(actual, false);
+                            actual = actual.right;
+                            nodes.Add(actual);
+                        }
+                        
+                        break;
+                    case ')':
+                        if (number != "")
+                        {
+                            setValue(number);
+                            number = "";
+                        }
+                        actual = actual.parent;
+                        break;
+                    default:
+                        number += c;
+                        break;
+                }
+            }
+            actual = actual.left;
+            sr.Close();
+            maxX = ((int)(Math.Pow(2, Node.maxDepth) + 3)) * (Node.width + 2* circleSize);
+            maxY = (Node.maxDepth + 2) * Node.height;
+
+            InitializeBitmap();
+            DrawNodes();
+            pictureBox1.Invalidate();
+        }
+
+        Brush nodeColor = Brushes.DarkOrange;
+        Pen lineColor = Pens.Black;
+
+        // Draw one node
+        private void setValue(string number)
+        {
+            
+            actual.doleva = false;
+
+            if (number.Length == 1)
+            {
+                number = "  " + number;
+            }
+            else if (number.Length == 2)
+            {
+                number = " " + number;
+            }
+
+            actual.value = number;
+
+        }
+
+        public void InitializeBitmap()
+        {
+            image = new Bitmap(maxX, maxY);
+            pictureBox1.Width = maxX;
+            pictureBox1.Height = maxY;
+            g = Graphics.FromImage(image);
+            pictureBox1.Image = image;
+
+            FontFamily fontFamily = new FontFamily("Arial");
+            font = new Font(
+               fontFamily,
+               20,
+               FontStyle.Bold,
+               GraphicsUnit.Pixel);
+        }
+
+        List<Node> nodes = new List<Node>();
+
+        public void DrawNodes()
+        {
+            if (nodes.Count == 1)
+            {
+                Node n = nodes[0];
+                g.FillEllipse(nodeColor, n.x, n.y, circleSize, circleSize);
+                g.DrawString(n.value.ToString(), font, Brushes.White, new Point((int)(n.x + (0 * circleSize)), (int)(n.y + (0.24 * circleSize))));
+            }
+            else
+            {
+                foreach (Node n in nodes)
+                {
+                    if (!n.parent.fake)
+                    {
+                        g.DrawLine(lineColor, new Point((int)(n.x + (0.5 * circleSize)), (int)(n.y + (0.5 * circleSize))), new Point((int)(n.parent.x + (0.5 * circleSize)), (int)(n.parent.y + (0.5 * circleSize))));
+                        g.FillEllipse(nodeColor, n.x, n.y, circleSize, circleSize);
+                        g.DrawString(n.value.ToString(), font, Brushes.White, new Point((int)(n.x + (0 * circleSize)), (int)(n.y + (0.24 * circleSize))));
+                        g.FillEllipse(nodeColor, n.parent.x, n.parent.y, circleSize, circleSize);
+                        g.DrawString(n.parent.value.ToString(), font, Brushes.White, new Point((int)(n.parent.x + (0.0 * circleSize)), (int)(n.parent.y + (0.24 * circleSize))));
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+            }
+        }
+    }
+
+
+    class Node
+    {
+        public static int height = 70;
+        public static int width = 50;
+        public static int maxDepth = -1;
+
+        public Node(Node parent, bool left)
+        {
+            this.parent = parent;
+
+            if (parent != null)
+            {
+                this.depth = parent.depth + 1;
+                if (this.depth > maxDepth)
+                {
+                    maxDepth = this.depth;
+                }
+
+                if (left)
+                {
+                    this.from = parent.from;
+                    this.to = parent.x;
+                    this.x = this.from + ((this.to - this.from) / 2);
+                    this.x -= 40;
+                }
+                else
+                {
+                    this.from = parent.x;
+                    this.to = parent.to;
+                    this.x = this.from + ((this.to - this.from) / 2);
+                    this.x += 40;
+                }
+
+                
+                this.y = parent.y + height;
+            }
+        }
+
+        public string value;
+        public Node left;
+        public Node right;
+        public Node parent;
+
+        public float x;
+        public float y;
+        public float from;
+        public float to;
+        public bool doleva = true;
+        public bool fake = false;
+        public int depth;
+    }
+}
